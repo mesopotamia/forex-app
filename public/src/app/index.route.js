4f2449c1d28faff51c5179962ec1dae37a37922a
Marwan Aziz <aziz.marwan@gmail.com>
2015-06-19 00:33:44 -0400

    deleted unnecessary boilerplate files

--- public/src/app/index.route.js
+++ public/src/app/index.route.js
@@ -13,9 +13,21 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('signon', {
+        url: '/signon',
+        templateUrl: 'app/signon/signon.html',
+        controller: 'SignOnController',
+        controllerAs: 'vm'
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'DashboardController',
+        controllerAs: 'vm'
       });
 
-    $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/dashboard');
   }
 
 })();

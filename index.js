e2805c89af8fe4cc48260eaf850d0c6fb2531533
Marwan Aziz <aziz.marwan@gmail.com>
2015-06-29 19:28:37 -0400

    minor fixes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var express = require("express"),
     headers = require("./libs/core/headers"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser"),
-    auth = require("./libs/authentication"),
     usersAPI = require("./api/users");
 
 mongoose.connect("mongodb://localhost/test");
@@ -14,7 +13,6 @@ app.use(bodyParser.urlencoded());
 
 app.use("/api", usersAPI);
 app.use("/rates", rates);
-app.use("/auth", bodyParser.urlencoded({ extended: true }), auth);
 
 app.get("/", function (req, res) {
     res.send("Welcome to my forex app");
